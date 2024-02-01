6b350293ec85810e5a2cbeb2cec4ecb296dd4b7c
Juandavidc08 <>
2024-02-01 10:27:13 +0000

    Fix game loop function , words doesnt repeat

--- run.py
+++ run.py
@@ -7,30 +7,39 @@ import random
 
 print("Welcome to The Language Game\n")
 print("Today we will test your basic Spanish knowledge\n")
-print("You will receive a set of 10 words, each featuring a different word. Your task is to translate these words from Spanish into English.")
-print("For every correct translation, you will earn a point towards your score. Try to complete all 10 questions in a consecutive manner.\n")
+print("You will receive a set of 10 words.")
+print("Your task is to translate them from Spanish into English.")
+print("For every correct translation, you will earn a point.")
+print("Try to complete all 10 questions in a consecutive manner.\n")
 answer = input("Are you ready to play the Quiz? (y/n): \n")
-score = 0 
+score = 0
 total_questions = 10
 
-# Function to read words from a file 
+# Function to read words from a file
+
+
 def search_words(file_name):
     with open(file_name, "r") as file:
         # Split the content into lines
         words = file.read().splitlines()
     return words
 
+
 words = search_words("words.txt")
 word = random.choice(words)
 
 # Function to find word translation in Spanish
+
+
 def translate_word(word):
     translation = translator.translate(word, src="es")
     return translation.text
 
 # Questions function
-def game_questions(question_number, word):
-    user_answer = input(f"Question {question_number}: How do you say in English '{word}'?\n")
+
+
+def game_questions(quest_num, word):
+    user_answer = input(f"{quest_num}:How do you say in English '{word}'?\n")
 
     translation = translate_word(word)
 
@@ -43,28 +52,37 @@ def game_questions(question_number, word):
         print(f"Wrong answer. The correct answer is {translation}")
         print(f"Your score is: {score} correct answers")
 
-#Question loop 
+# Question loop
+
+
 def game_loop():
-    # Variable to identifly which words have already being seen in the game an remove them so it doesnt repeat
+    """
+     Variable to identifly which words have already being
+     seen in the game an remove them so it doesnt repeat
+     """
     remaining_words = list(words)
 
-    #range of values that question_number will take. It will starts from 1 and goes up to 10
-    for question_number in range(1, total_questions + 1):
+    """
+    range of values that quest_number will take
+    It will starts from 1 and goes up to 10
+    """
+    for quest_num in range(1, total_questions + 1):
         if not remaining_words:
             print("Error: Not enough words in the list.")
             break
 
-        word = random.choice(words)
-        game_questions(question_number, word)
-        #take out the last word that it is used so it doesnt appear again NEED TO FIX!!!!
-        word = remaining_words.pop()
-        
-       
+        word = random.choice(remaining_words)
+        game_questions(quest_num, word)
+        # take out the last word that it is used so it doesn't appear again
+        remaining_words.remove(word)
+
 # Google API translator
+
+
 translator = Translator()
 
-#Start Game
-if answer.lower() == "y" :
+# Start Game
+if answer.lower() == "y":
     game_loop()
 else:
     print("You did not choose 'yes.' Exiting the game.")
